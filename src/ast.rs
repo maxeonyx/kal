1d2b090bbaecb79e722fd14fa65216b6c04fbc61
Maxwell Clarke <maxeonyx@gmail.com>
2019-01-15 13:53:06 +1300

    Improve grammar to be more like a real language

--- src/ast.rs
+++ src/ast.rs
@@ -1,13 +1,47 @@
-#[derive(Debug)]
-pub enum Statement {
-	Input,
-	Print(Box<Expression>),
-}
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub enum Expression {
-	True,
-	False,
-	And(Box<Expression>, Box<Expression>),
-	Or(Box<Expression>, Box<Expression>),
+	Literal(Box<Literal>),
+	FunctionInvocation(Box<FunctionInvocation>),
+	Let(Box<LetExpression>),
+	If(Box<IfExpression>),
 }
+
+#[derive(Debug)]
+pub struct LetExpression(pub Box<Ident>, pub Box<Expression>);
+
+#[derive(Debug)]
+pub struct IfExpression(pub Box<Expression>, pub Box<Block>);
+
+#[derive(Debug)]
+pub enum Literal {
+	Null,
+	Bool(bool),
+	Int(i64),
+	String(String),
+	Object(Box<Object>),
+	List(Box<List>),
+	Function(Box<Function>),
+}
+
+#[derive(Debug)]
+pub struct Function {
+	pub parameters: Vec<Ident>,
+	pub body: Block,
+}
+
+#[derive(Debug)]
+pub struct Block(pub Vec<Expression>);
+
+#[derive(Debug)]
+pub struct FunctionInvocation(pub Box<Ident>, pub Vec<Expression>);
+
+#[derive(Debug)]
+pub struct Object(pub HashMap<Ident, Expression>);
+
+#[derive(Debug)]
+pub struct List(pub Vec<Expression>);
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct Ident(pub String);
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ lalrpop_mod!(pub kal_grammar);
 
 fn main() {
     println!(
-        "{:?}",
-        kal_grammar::Function_BodyParser::new().parse(include_str!("./fake_example.kal"))
+        "{:#?}",
+        kal_grammar::BlockInnerParser::new().parse(include_str!("./fake_example.kal"))
     );
 }
