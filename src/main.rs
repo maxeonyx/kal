907ba6080b123f6ebacfacaf54e011e858895c7b
Max Clarke <maxeonyx@gmail.com>
2023-09-17 15:15:36 +1200

    Add basic CLI (about time!)

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,30 @@ mod eval_impls;
 mod interpreter;
 mod intrinsics;
 
+use std::path::PathBuf;
+
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(#[allow(clippy::all)] pub kal_grammar);
 
 use interpreter::Interpreter;
 
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    filename: PathBuf,
+}
+
 fn main() {
+
+    let args = Args::parse();
+    let borrowed_path = args.filename.as_path();
+    let file = std::fs::read_to_string(borrowed_path).unwrap_or_else(|err| panic!("Failed to read file {:}. Error: {:?}", borrowed_path.to_string_lossy(), err));
+
     let ast = kal_grammar::BlockInnerParser::new()
-        .parse(include_str!("../tests/fibonacci.kal"))
+        .parse(file.as_str())
         .unwrap_or_else(|err| panic!("Failed to parse file, {:?}", err));
 
     let mut interpreter = Interpreter::new();
